402aab0b0426a7cfa3bfb9315eb44d59c32c3ee5
danm <writetodan@yahoo.com>
2013-06-09 23:45:07 +0100

    @damned splitting out example tokeniser - delegates to other tokenizers

--- lib/example_tokenizer.rb
+++ lib/example_tokenizer.rb
@@ -1,10 +1,22 @@
 class ExampleTokenizer
   def initialize(specified_token_extractor, inferred_token_extractor)
-    @specified_token_extractor = specified_token_extractor
-    @inferred_token_extractor = inferred_token_extractor
+    @tokenizers = [ specified_token_extractor, inferred_token_extractor ]
   end
 
   def tokenize(example, specified_tokens)
-    @specified_token_extractor.tokenize(example, specified_tokens)
+    example_parts = [ example ]
+    tokens = keys_as_strings(specified_tokens)
+    @tokenizers.each do |tokenizer|
+      example_parts, tokens = tokenizer.tokenize(example_parts, tokens)
+    end
+    [ example_parts, tokens ]
+  end
+
+  def keys_as_strings(hash)
+    newhash = {}
+    hash.each { |k, v|
+      newhash[k.to_s] = v
+    }
+    newhash
   end
 end
--- spec/tokenizers_spec.rb
+++ spec/tokenizers_spec.rb
@@ -1,39 +1,62 @@
 require 'rspec'
 require 'rspec/mocks'
 require_relative '../lib/example_tokenizer'
-require_relative '../lib/specified_token_extractor'
-require_relative '../lib/inferred_token_extractor'
+
 
 describe ExampleTokenizer do
   describe '#tokenize' do
     before :each do
-      @specified_token_extractor = double(SpecifiedTokenExtractor)
-      @inferred_token_extractor = double(InferredTokenExtractor)
-      @example_tokenizer = ExampleTokenizer.new(@specified_token_extractor, @inferred_token_extractor)
+      @extractors = [double('first extractor'), double('second extractor')]
+      @example_tokenizer = ExampleTokenizer.new(*@extractors)
       @specifier = double('Specifier')
       @another_specifier = double('Specifier')
     end
 
-    it 'should extract specified tokens if composed entirely of specified tokens' do
-      specified_tokens = {'some' => @specifier, 'example' => @another_specifier}
-      @specified_token_extractor.stub(tokenize: specified_tokens)
-      @inferred_token_extractor.should_not_receive(:tokenize)
-      tokens = @example_tokenizer.tokenize('someexample', {
+    it 'should pass ' do
+      specified_tokens = {
+          'some' => @specifier,
+          'example' => @another_specifier
+      }
+
+      @extractors[0].should_receive(:tokenize).
+          with(['someexample'], specified_tokens).
+          and_return([['some', 'example'], specified_tokens ])
+      stub_to_pass_through(@extractors[1])
+
+      tokens, token_specifications = @example_tokenizer.tokenize('someexample', {
           some: @specifier,
           example: @another_specifier
       })
-      tokens.should be(specified_tokens)
+
+      tokens.should eq(['some', 'example'])
+      token_specifications.should eq(specified_tokens)
     end
 
-    it 'should extract unspecified, inferred tokens'
+    it 'should extract unspecified, inferred tokens if composed entirely of inferred tokens' do
+      @extractors[0].stub(tokenize: [ ['testcase'], { } ])
+      @extractors[1].stub(tokenize: [ ['testcase'], { 'testcase' => :some_specifier } ])
+
+      tokens, token_specifications = @example_tokenizer.tokenize('testcase', {})
+
+      tokens.should eq(['testcase'])
+      token_specifications.should eq('testcase' => :some_specifier)
+    end
+
     it 'should extract a mix of specified and inferred tokens'
   end
 end
-describe SpecifiedTokenExtractor do
+
+def stub_to_pass_through(extractor)
+  extractor.stub(:tokenize) do |tokens, specified|
+    [tokens, specified]
+  end
+end
+
+describe 'SpecifiedTokenExtractor' do
   it 'should find specified tokens in an example string'
 end
 
-describe InferredTokenExtractor do
+describe 'InferredTokenExtractor' do
   it 'should infer tokens in example based on heuristic of consecutive characters of the same type'
   it 'should tokenize an array of multiple example parts into an array of token strings'
 end
