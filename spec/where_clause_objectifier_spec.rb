2f0b20fd507f89ec98873d2a0f1fe5b4974a03cc
danm <writetodan@yahoo.com>
2013-06-16 13:57:13 +0100

    @damned funky colour if can run rspec outside rake

--- spec/where_clause_objectifier_spec.rb
+++ spec/where_clause_objectifier_spec.rb
@@ -2,15 +2,16 @@ require 'rspec'
 require_relative '../lib/where_clause_objectifier'
 
 describe WhereClauseObjectifier do
-  describe '#objectify' do
-    it 'should replace hash of specifier symbol values with Specifier values'
-    describe '#one_of' do
-      it 'should parse each character of a single string to an Alternatives'
-    end
-    describe '#may_be' do
-      it 'should (handle alternate option)'
-      it 'should use #or_else as an alias'
-    end
-  end
+  pending 'not yet implemented'
+  #describe '#objectify' do
+  #  it 'should replace hash of specifier symbol values with Specifier values'
+  #  describe '#one_of' do
+  #    it 'should parse each character of a single string to an Alternatives'
+  #  end
+  #  describe '#may_be' do
+  #    it 'should (handle alternate option)'
+  #    it 'should use #or_else as an alias'
+  #  end
+  #end
 end
 
