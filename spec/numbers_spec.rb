4a10fc30b1498fac2d7bca4b1aa1973bc40c2207
danm <writetodan@yahoo.com>
2013-08-11 18:49:31 +0100

    @damned better spec structure

--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -4,20 +4,24 @@ describe 'number matching' do
 
   include Egrex
 
-  it 'should match a number with a digit example' do
-    eg('1').match('9').ok.should be_true
+  describe '1 digit example' do
+    it 'should match a number with a digit example' do
+      eg('1').match('9').ok.should be_true
+    end
+
+    it 'should not match a character with a digit example' do
+      eg('2').match('x').ok.should be_false
+    end
   end
 
-  it 'should not match a character with a digit example' do
-    eg('2').match('x').ok.should be_false
+  describe '2 digit example' do
+    it 'should match 2 numbers with a 2-digit example' do
+      eg('12').match('67').ok.should be_true
+    end
+
+    it 'should not match 1 number with a 2-digit example' do
+      eg('12').match('6').ok.should be_false
+    end
   end
 
-
-  it 'should match 2 numbers with a 2-digit example' do
-    eg('12').match('67').ok.should be_true
-  end
-
-  it 'should not match 1 number with a 2-digit example' do
-    eg('12').match('6').ok.should be_false
-  end
 end
